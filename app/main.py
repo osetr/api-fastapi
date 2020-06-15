76cb7d2883d219d6380b170cbb2981e4dc80283d
anton <uawesome13@gmail.com>
2020-06-15 17:49:52 +0300

    status codes adjusted
    done with refactoring

--- app/main.py
+++ app/main.py
@@ -34,21 +34,30 @@ def read_users(limit: int = 100, db: Session = Depends(get_db)):
 async def read_users_me(current_user = Depends(crud.get_current_user)):
     return current_user
 
-@app.post("/users/new_user", response_model=schemas.UserCreate)
+@app.post("/users/new_user", response_model=schemas.UserCreate, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_login(db, login=user.login)
     if db_user:
-        raise HTTPException(status_code=400, detail="Login already registered")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Login already registered"
+        )
     return crud.create_user(db=db, user=user)
 
 @app.get("/users/{user_login}/last_request/", response_model=schemas.LastRequest)
 def read_posts(user_login: str, db: Session = Depends(get_db), current_user = Depends(crud.get_current_user)):
     print(user_login)
     if user_login != current_user.current_user:
-        raise HTTPException(status_code=400, detail="User_login not found!")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="There're no permissions for user " + current_user.current_user
+        )
     db_user = crud.get_user_by_login(db, login=user_login)
     if not db_user:
-        raise HTTPException(status_code=400, detail="User_login not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User_login not found"
+        )
     return crud.last_request(db, login=user_login)
 
 
@@ -61,16 +70,25 @@ def read_posts(limit:int = 100, db: Session = Depends(get_db)):
 def read_posts(user_login: str = None, limit:int = 100, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_login(db, login=user_login)
     if not db_user:
-        raise HTTPException(status_code=400, detail="User_login not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User_login not found"
+        )
     return crud.get_certain_posts(db, login=user_login, limit=limit)
 
 @app.post("/users/{user_login}/posts/create/", response_model=schemas.PostCreate)
 def create_post(user_login: str = None, current_user = Depends(crud.get_current_user), post: schemas.PostCreate = None, db: Session = Depends(get_db)):
     if user_login != current_user.current_user:
-        raise HTTPException(status_code=400, detail="User_login not found")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="There're no permissions for user " + current_user.current_user
+        )
     db_user = crud.get_user_by_login(db, user_login)
     if not db_user:
-        raise HTTPException(status_code=400, detail="Faulty user_login")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User_login not found"
+        )
     return crud.create_post(db=db, post=post, user_login=user_login)
 
 
@@ -78,40 +96,70 @@ def create_post(user_login: str = None, current_user = Depends(crud.get_current_
 @app.post("/users/{user_login}/likes/", response_model=schemas.Like)
 def like(user_login: str, post_id: int, db: Session = Depends(get_db), current_user = Depends(crud.get_current_user)):
     if user_login != current_user.current_user:
-        raise HTTPException(status_code=400, detail="User_login not found")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="There're no permissions for user " + current_user.current_user
+        )
     db_user = crud.get_user_by_login(db, user_login)
     if not db_user:
-        raise HTTPException(status_code=400, detail="Faulty user_login")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User_login not found"
+        )
     db_post = crud.get_post_by_id(db, post_id)
     if not db_post:
-        raise HTTPException(status_code=400, detail="Post not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Post not found"
+        )
     try:
         return crud.like(db=db, user_login=user_login, post_id=post_id)
     except:
-        raise HTTPException(status_code=409, detail="Probably like already exist")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Probably like already exists"
+        )
 
 @app.delete("/users/{user_login}/unlikes/", response_model=schemas.Like)
 def unlike(user_login: str, post_id: int, db: Session = Depends(get_db), current_user = Depends(crud.get_current_user)):
     if user_login != current_user.current_user:
-        raise HTTPException(status_code=400, detail="User_login not found")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="There're no permissions for user " + current_user.current_user
+        )
     db_user = crud.get_user_by_login(db, user_login)
     if not db_user:
-        raise HTTPException(status_code=400, detail="Faulty user_login")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User_login not found"
+        )
     db_post = crud.get_post_by_id(db, post_id)
     if not db_post:
-        raise HTTPException(status_code=400, detail="Post not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Post not found"
+        )
     try:
         return crud.unlike(db=db, user_login=user_login, post_id=post_id)
     except:
-        raise HTTPException(status_code=409, detail="Probably user still unlike this post")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Probably user still unlikes post"
+        )
 
 @app.get("/likes_info/", response_model=schemas.LikeList)
 def likes_info(post_id: int, db: Session = Depends(get_db), date_from: str = "2000-12-31", date_to: str = "2020-12-12"):
     db_post = crud.get_post_by_id(db, post_id)
     if not db_post:
-        raise HTTPException(status_code=400, detail="Post not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Post not found"
+        )
     if not (re.search("^20[0-9][0-9](-)(0[1-9]|1[0-2])(-)(0[1-9]|1[0-9]|2[0-9]|3[0-1])$",date_from) and (re.search("^20[0-9][0-9](-)(0[1-9]|1[0-2])(-)(0[1-9]|1[0-9]|2[0-9]|3[0-1])$",date_to))):
-        raise HTTPException(status_code=400, detail="Uncorrect date")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Uncorrect date input"
+        )
     return crud.likes_info(post_id=post_id, db=db, date_from=date_from, date_to=date_to)
 
 
